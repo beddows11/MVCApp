4227b2e6d206f0edf293b50457130b0dee740e43
david.beddows <david.beddows@SYC0047>
2016-06-09 15:07:05 +0100

    Another test commit

--- MVCApp/Models/Movie.cs
+++ MVCApp/Models/Movie.cs
@@ -11,7 +11,7 @@ namespace MvcMovie.Models
 
         [Display(Name = "Release Date")]
         [DataType(DataType.Date)]
-        [DisplayFormat(DataFormatString = "{0:00000000d}", ApplyFormatInEditMode = true)]
+        [DisplayFormat(DataFormatString = "{0:d}", ApplyFormatInEditMode = true)]
 
         public DateTime ReleaseDate { get; set; }
         public string Genre { get; set; }
